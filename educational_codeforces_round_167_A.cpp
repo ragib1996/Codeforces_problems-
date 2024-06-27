7a35021986c7f48f4930fd6bc781deab5cd5fa02
ragib1996 <46626132+ragib1996@users.noreply.github.com>
2024-06-27 22:12:35 +0600

    Add files via upload

---
+++ educational_codeforces_round_167_A.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+
+int main()
+{
+    int i,j,k,n;
+    scanf("%d", &n);
+    int a[n][2];
+    
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d %d", & a[i][0], & a[i][1]);
+    }
+    
+    for (i = 0; i < n ; i++ )
+    {
+        if ( a[i][1] >= -1)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
